6fbd68840e07b225ad366bb760adc95a16a1e0ae
Kiran <kiranpuri532@gmail.com>
2023-07-18 08:34:48 +0530

    [Kiran]:\ it will handle diveded by error

---
+++ ExceptionHandleDemo/ExceptionHandleDemo/DividedByZero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandleDemo
+{
+    public class DividedByZero
+    {
+        public void Divided_By_Zero()
+        {
+            //It will handle error generated from divided by zero.
+            int x = 0;
+            try
+            {
+                int a = 100 / x;
+
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
--- ExceptionHandleDemo/ExceptionHandleDemo/Program.cs
+++ ExceptionHandleDemo/ExceptionHandleDemo/Program.cs
@@ -10,8 +10,25 @@ namespace ExceptionHandleDemo
     {
         static void Main(string[] args)
         {
-            HandleException handleException = new HandleException();
-            handleException.Display();
+            while(true)
+            {
+                Console.WriteLine("1- NormalizationForm Exception");
+                Console.WriteLine("2- Devided by zero");
+                Console.WriteLine("Choose above option");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch(option)
+                {
+                    case 1:
+                        HandleException handleException = new HandleException();
+                        handleException.Display();
+                        break;
+                    case 2:
+                        DividedByZero dividedByZero = new DividedByZero();
+                        dividedByZero.Divided_By_Zero();
+                            break;
+                }
+            }
+            
         }
     }
 }
